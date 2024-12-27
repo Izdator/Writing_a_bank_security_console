25fb66a899d7abba7facc15adf883e954ca81c85
Izdator <evarborisov@gmail.com>
2024-12-28 02:08:56 +0300

    Update storage_information_view.py
    More precise names of functions have been chosen.

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -5,18 +5,17 @@ from django.utils import timezone
 
 
 def storage_information_view(request):
-    non_closed_visits = Visit.objects.filter(leaved_at=None)
+    open_visits = Visit.objects.filter(leaved_at=None)
 
-    visits_data = []
-    for visit in non_closed_visits:
-        visits_data.append({
+    open_visits_data = []
+    for visit in open_visits:
+        open_visits_data.append({
             'who_entered': visit.passcard.owner_name,
             'entered_at': visit.entered_at.strftime('%d-%m-%Y %H:%M'),
             'duration': str(timezone.now() - visit.entered_at),
         })
 
     context = {
-        'non_closed_visits': visits_data,
+        'open_visits': open_visits_data,
     }
     return render(request, 'storage_information.html', context)
-
